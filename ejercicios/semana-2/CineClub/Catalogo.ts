4424e171ea3f1838de1e7f71369a9a98c5c96c8f
Isac <isaacalvarez0516@gmail.com>
2026-08-05 12:26:35 -0500

    avance de la actividad

--- ejercicios/semana-2/CineClub/Catalogo.ts
+++ ejercicios/semana-2/CineClub/Catalogo.ts
@@ -16,29 +16,79 @@ class Pelicula {
     }
 
     mostrarInformacion(){
+        console.log("");
+        console.log("-------------------------------");
+        console.log("");
         console.log(this.Título);
         console.log(`Director: ${this.Director}`);
         console.log(`Año: ${this.Anio}`);
         console.log(`Género: ${this.Genero}`);
         console.log(`⭐: ${this.Calificación}/10`);
+
+        
+        
     }
 
-    esRecomendada(Calificion: number) {
-        Calificion = 8
-}
+    esRecomendada() {
+        if (this.Calificación >= 8) {
+            console.log(`${this.Título} ⭐: ${this.Calificación}`);
+        } else{
+            return console.log(false);
+            
+        }
+    }
+
+    esLarga(){
+        if (this.Duracion > 120){
+            return console.log(true);
+        } else{
+            return console.log(false);
+        }
+    }
+
+    actualizarCalificacion(calificionNueva: number){
+        if (calificionNueva > 10){
+            console.log("La calificación debe estar entre 0 y 10.");
+        }else if(calificionNueva < 0){
+            console.log("La calificación no debe ser menor a 0");
+        }else{
+            this.Calificación = calificionNueva
+            console.log(`La calificacion de la pelicula ${this.Título} se actualizo a ${calificionNueva}`);
+            
+        }
+    }
+
 }
 
 const pelicula1 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
 
-const pelicula2 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
+const pelicula2 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",6);
 
-const pelicula3 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
+const pelicula3 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",5);
 
-const pelicula4 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
+const pelicula4 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",8);
 
-const pelicula5 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
+const pelicula5 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",3);
 
-const pelicula6 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
+const pelicula6 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",7);
 
-const pelicula7 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
+const pelicula7 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",5);
 
+const pelicula8 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",2);
+
+const peliculas: Pelicula[] = [pelicula1,pelicula2,pelicula3,pelicula4,pelicula5,pelicula6,pelicula7,pelicula8]
+
+peliculas.map( (item) => {
+    item.mostrarInformacion()
+    console.log("-------------------------------");
+    item.esRecomendada()
+}
+)
+
+// pelicula5.mostrarInformacion()
+
+// pelicula5.esRecomendada()
+
+// pelicula5.esLarga()
+
+// pelicula5.actualizarCalificacion(7)
