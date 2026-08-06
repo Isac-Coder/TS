a493fb5820da6fc2b6406d0623135b0a108f60e7
Isac <isaacalvarez0516@gmail.com>
2026-08-06 12:44:55 -0500

    update files

--- ejercicios/semana-2/CineClub/Catalogo.ts
+++ ejercicios/semana-2/CineClub/Catalogo.ts
@@ -1,86 +1,4 @@
-class Pelicula {
-        Título: string;
-        Anio: string ;
-        Genero: string ;
-        Duracion: number ;
-        Director: string ;
-        Calificación: number ;
-
-    constructor(Titulo: string, Anio: string, Genero: string, Duracion: number, Director: string, Calificion: number) {
-        this.Título = Titulo
-        this.Anio = Anio
-        this.Genero = Genero
-        this.Duracion = Duracion
-        this.Director = Director
-        this.Calificación = Calificion
-    }
-
-    mostrarInformacion(){
-        console.log("");
-        console.log("-------------------------------");
-        console.log("");
-        console.log(this.Título);
-        console.log(`Director: ${this.Director}`);
-        console.log(`Año: ${this.Anio}`);
-        console.log(`Género: ${this.Genero}`);
-        console.log(`⭐: ${this.Calificación}/10`);
-
-        
-        
-    }
-
-    esRecomendada() {
-        if (this.Calificación >= 8) {
-            console.log(`${this.Título} ⭐: ${this.Calificación}`);
-        } else{
-            return console.log(false);
-            
-        }
-    }
-
-    esLarga(){
-        if (this.Duracion > 120){
-            return console.log(true);
-        } else{
-            return console.log(false);
-        }
-    }
-
-    actualizarCalificacion(calificionNueva: number){
-        if (calificionNueva > 10){
-            console.log("La calificación debe estar entre 0 y 10.");
-        }else if(calificionNueva < 0){
-            console.log("La calificación no debe ser menor a 0");
-        }else{
-            this.Calificación = calificionNueva
-            console.log(`La calificacion de la pelicula ${this.Título} se actualizo a ${calificionNueva}`);
-            
-        }
-    }
-
-    buscarPelicula(titulo: string){
-        if (this.Título == titulo){
-            console.log("Película encontrada:");
-            console.log(this.Título);
-        }else if(titulo != this.Título){
-            console.log("Película no encontrada");
-            
-        }
-    }
-
-    buscarPorGenero(genero: string){
-        if (this.Genero == genero){
-            console.log(this.Título);
-        }else{
-            console.log(`No se encontro el genero: ${genero}`);
-            
-        }
-    }
-
-}
-
 class ContenidoAudiovisual {
-            
             titulo: string ;
             anio: number ;
             duracion: number ;
@@ -92,23 +10,120 @@ class ContenidoAudiovisual {
                 this.duracion = duracion
                 this.calificacion = calificacion
             }
+
+                mostrarInformacion(){
+        console.log("");
+        console.log("-------------------------------");
+        console.log("");
+        console.log(this.titulo);
+        console.log(`Año: ${this.anio}`);
+        console.log(`⭐: ${this.calificacion}/10`);
+    }
+}
+
+class Documental extends ContenidoAudiovisual {
+
+    tema: string;
+    narrador: string;
+
+    constructor(titulo: string, anio: number, duracion: number, calificion: number,tema: string, narrador: string){
+        super(titulo,anio,duracion,calificion)
+        this.tema = tema;
+        this.narrador = narrador
+    }
+}
+
+class Pelicula extends ContenidoAudiovisual{
+
+        genero: string ;
+        director: string ;
+
+    constructor(titulo: string, anio: number, genero: string, duracion: number, director: string, calificion: number) {
+        super(titulo,anio,duracion,calificion)
+
+        this.genero = genero ;
+        this.director = director ;
+
+    
+    }
+
+    mostrarInformacion(){
+        console.log("");
+        console.log("-------------------------------");
+        console.log("");
+        console.log(this.titulo);
+        console.log(`director: ${this.director}`);
+        console.log(`Año: ${this.anio}`);
+        console.log(`Género: ${this.genero}`);
+        console.log(`⭐: ${this.calificacion}/10`);
+    }
+
+    esRecomendada() {
+        if (this.calificacion >= 8) {
+            console.log(`${this.titulo} ⭐: ${this.calificacion}`);
+        } else{
+            return console.log(false);
+            
         }
+    }
 
-const pelicula1 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",9);
+    esLarga(){
+        if (this.duracion > 120){
+            return console.log(true);
+        } else{
+            return console.log(false);
+        }
+    }
 
-const pelicula2 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",6);
+    actualizarcalificacion(calificionNueva: number){
+        if (calificionNueva > 10){
+            console.log("La calificacion debe estar entre 0 y 10.");
+        }else if(calificionNueva < 0){
+            console.log("La calificacion no debe ser menor a 0");
+        }else{
+            this.calificacion = calificionNueva
+            console.log(`La calificacion de la pelicula ${this.titulo} se actualizo a ${calificionNueva}`);
+            
+        }
+    }
 
-const pelicula3 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",5);
+    buscarPelicula(titulo: string){
+        if (this.titulo == titulo){
+            console.log("Película encontrada:");
+            console.log(this.titulo);
+        }else if(titulo != this.titulo){
+            console.log("Película no encontrada");
+            
+        }
+    }
 
-const pelicula4 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",8);
+    buscarPorgenero(genero: string){
+        if (this.genero == genero){
+            console.log(this.titulo);
+        }else{
+            console.log(`No se encontro el genero: ${genero}`);
+            
+        }
+    }
 
-const pelicula5 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",3);
+}
 
-const pelicula6 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",7);
 
-const pelicula7 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",5);
+const pelicula1 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",9);
 
-const pelicula8 = new Pelicula("Interstellar","2014","Ciencia ficción",169,"Christopher Nolan",2);
+const pelicula2 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",6);
+
+const pelicula3 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",5);
+
+const pelicula4 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",8);
+
+const pelicula5 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",3);
+
+const pelicula6 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",7);
+
+const pelicula7 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",5);
+
+const pelicula8 = new Pelicula("Interstellar",2014,"Ciencia ficción",169,"Christopher Nolan",2);
 
 const peliculas: Pelicula[] = [pelicula1,pelicula2,pelicula3,pelicula4,pelicula5,pelicula6,pelicula7,pelicula8]
 
@@ -119,7 +134,7 @@ peliculas.map( (item) => {
     console.log("-------------------------------");
     item.buscarPelicula("Inter")
     console.log("-------------------------------");
-    item.buscarPorGenero("Ciencia")
+    item.buscarPorgenero("Ciencia")
 }
 )
 
@@ -129,4 +144,4 @@ peliculas.map( (item) => {
 
 // pelicula5.esLarga()
 
-// pelicula5.actualizarCalificacion(7)
+// pelicula5.actualizarcalificacion(7)
